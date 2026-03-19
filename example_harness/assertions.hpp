b5292cc6aae9521f8eec763d56d76470fbab233c
Nora <nhinderling@student.ethz.ch>
2026-03-19 15:48:13 +0100

    assertion layout

---
+++ example_harness/assertions.hpp
@@ -0,0 +1,16 @@
+#ifndef _ASSERTIONS_HPP
+#define _ASSERTIONS_HPP
+
+
+template <typename snapshot_t>
+inline bool assert_fn(const snapshot_t& snap) {
+  return true;
+}
+
+template <typename snapshot_t>
+inline bool assert_final(const harness::snapshot_t& snap) {
+   return true; 
+}
+
+
+#endif
